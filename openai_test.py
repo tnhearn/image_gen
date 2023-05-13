2ab3eb67772802eeee4b4a11aea2cecee795f3ab
Taylor Hearn <71198378+tnhearn@users.noreply.github.com>
2023-05-13 11:15:56 -0600

    Hid API key

--- openai_test.py
+++ openai_test.py
@@ -5,12 +5,13 @@ Created on Fri Apr 28 13:37:04 2023
 
 @author: thear
 """
-import os
+# import os
 import openai
 
 
+with open("/Users/thear/Documents/openai_key.txt", "r") as file:
+    openai.api_key = file.read()
 
-openai.api_key = "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"
 # openai.Model.list()
 
 
